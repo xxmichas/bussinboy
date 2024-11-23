23f8059439904759604e3cc23bce56f1f9b0fc51
Michał Orkisz <xxmichas@gmail.com>
2024-11-23 14:29:32 +0100

    added more tests

---
+++ .changeset/sixty-adults-matter.md
@@ -0,0 +1,5 @@
+---
+"@xxmichas/bussinboy": patch
+---
+
+added more tests
--- test/tests/invalid-data.test.mjs
+++ test/tests/invalid-data.test.mjs
@@ -74,14 +74,27 @@ describe("invalid or malformed data", () => {
   });
 
   it("should ignore data if Content-Disposition header is missing", async () => {
-    const stream = createMultipartFormDataStream(
-      `--${boundary}\r\nname="test-name"\r\n\r\ntest-value\r\n--${boundary}--`,
+    const stream1 = createMultipartFormDataStream(
+      `--${boundary}\r\nname="test-name"\r\n\r\ntest-value\r\n--${boundary}--`, // Ignored
     );
 
-    const data = await bussinboy({ headers }, stream);
+    const data1 = await bussinboy({ headers }, stream1);
 
-    assert.deepEqual(data, {
+    assert.deepEqual(data1, {
       fields: [],
+      files: [],
+    });
+
+    const stream2 = createMultipartFormDataStream(
+      createMultipartFieldChunk("name1", "value1"),
+      `--${boundary}\r\nname="test-name"\r\n\r\ntest-value\r\n--${boundary}--`, // Ignored
+      createMultipartFieldChunk("name2", "value2"), // Ignored
+    );
+
+    const data2 = await bussinboy({ headers }, stream2);
+
+    assert.deepEqual(data2, {
+      fields: [{ encoding: "7bit", mimeType: "text/plain", name: "name1", value: "value1" }],
       files: [],
     });
   });
@@ -100,6 +113,32 @@ describe("invalid or malformed data", () => {
     }
   });
 
+  it(`should set mimeType to "text/plain" if file Content-Type header is missing`, async () => {
+    const fieldName = "test-name";
+    const fileName = "test-file-name";
+    const fileContent = "test-file-content";
+    const stream = createMultipartFormDataStream(
+      createMultipartFieldChunk("name1", "value1"),
+      [
+        `--${boundary}`,
+        `Content-Disposition: form-data; name="${fieldName}"; filename="${fileName}"`,
+        "",
+        fileContent,
+      ].join("\r\n"),
+      createMultipartFieldChunk("name2", "value2"),
+    );
+
+    const data = await bussinboy({ headers }, stream);
+
+    assert.deepEqual(data, {
+      fields: [
+        { encoding: "7bit", mimeType: "text/plain", name: "name1", value: "value1" },
+        { encoding: "7bit", mimeType: "text/plain", name: "name2", value: "value2" },
+      ],
+      files: [{ fieldName, fileName, encoding: "7bit", mimeType: "text/plain", buffer: Buffer.from(fileContent) }],
+    });
+  });
+
   it("should throw an error if body is too large", async () => {
     const fieldName = "test-name";
     const fileName = "test-file-name";
@@ -107,15 +146,7 @@ describe("invalid or malformed data", () => {
     const stream1 = createMultipartFormDataStream(createMultipartFileChunk(fieldName, fileName, fileContent));
     const bodySize = 186;
 
-    const data1 = await bussinboy(
-      {
-        limits: {
-          bodySize: bodySize,
-        },
-        headers,
-      },
-      stream1,
-    );
+    const data1 = await bussinboy({ limits: { bodySize: bodySize }, headers }, stream1);
 
     assert.deepEqual(data1, {
       fields: [],
